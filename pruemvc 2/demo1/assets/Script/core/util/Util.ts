86474fdb3dc42bb16fed749a95971e59aa3637be
nick <hanxingchen1986@gmail.com>
2024-03-22 15:48:51 +0800

    1

--- pruemvc 2/demo1/assets/Script/core/util/Util.ts
+++ pruemvc 2/demo1/assets/Script/core/util/Util.ts
@@ -10,4 +10,29 @@ export default class Util {
     public static getPureFacade(name: string): puremvc.IFacade {
         return puremvc.Facade.getInstance(name);
     }
+
+    /**
+     * 新增加的操作
+     * @param url 
+     * @param type 
+     * @param callbacks 
+     */
+    public static loadAsset(url: string, type: any, callbacks) {
+        cc.resources.load(url, type, (error, asset) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            callbacks(error, asset);
+        })
+    }
+
+    /**
+     * 清空数据
+     * @param url 
+     * @param type 
+     */
+    public static removeAsset(url: string, type: any) {
+        cc.resources.release(url, type);
+    }
 }
--- pruemvc 2/demo1/assets/Script/view/ui/DataEvent.ts
+++ pruemvc 2/demo1/assets/Script/view/ui/DataEvent.ts
@@ -4,11 +4,22 @@ export default class DataEvent {
 
     private text: cc.Label = null;
     private btn: cc.Button = null;
-
+    private UI: cc.Node = null;
+    /**
+     * 
+     * @param root 
+     */
     regUiEvent(root: cc.Node) {
-        this.text = root.getChildByName("lable").getComponent(cc.Label);
-        this.btn = root.getChildByName("addNum").getComponent(cc.Button);
-        this.btn.node.on('click', this.clickCallBack, this);
+        this.UI = root.getChildByName("UI");
+        this.UI.removeAllChildren();
+        Util.removeAsset("/test", cc.Prefab);
+        Util.loadAsset("/test", cc.Prefab, (error, assets) => {
+            let nodes: cc.Node = cc.instantiate(assets as cc.Prefab);
+            this.UI.addChild(nodes);
+            this.text = nodes.getChildByName("lable").getComponent(cc.Label);
+            this.btn = nodes.getChildByName("addNum").getComponent(cc.Button);
+            this.btn.node.on('click', this.clickCallBack, this);
+        })
     }
 
     //事件
