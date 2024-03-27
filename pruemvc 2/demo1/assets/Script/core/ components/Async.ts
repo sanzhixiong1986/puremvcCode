ecafcd2e123b80b0def1a1f9c7276f340a2a89a3
nick <hanxingchen1986@gmail.com>
2024-03-27 19:36:34 +0800

    添加load的添加

---
+++ pruemvc 2/demo1/assets/Script/core/ components/Async.ts
@@ -0,0 +1,39 @@
+/**
+ * 无参数函数
+ */
+type FuncWithoutParamz = (funYesContinue: () => void) => void
+
+export default class Async {
+    /**
+     * 私有化类默认构造器
+     */
+    private constructor() {
+    }
+    /**
+     * 顺序调用
+     * 
+     * @param oFunQ 函数队列
+     */
+    static serial(...oFunQ: Array<FuncWithoutParamz>): void {
+        if (!oFunQ ||
+            oFunQ.length <= 0) {
+            return;
+        }
+        // 循环函数
+        let funYesContinue = () => {
+            if (null == oFunQ ||
+                oFunQ.length <= 0) {
+                return;
+            }
+            // 函数对象出队
+            let funCurr = oFunQ.shift();
+            if (!funCurr) {
+                funYesContinue();
+                return;
+            }
+            // 将回调函数作为参数
+            funCurr(funYesContinue);
+        };
+        funYesContinue();
+    }
+}
