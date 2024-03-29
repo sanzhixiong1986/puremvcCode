e11abf4d989ac6ca2781ba20ea88cdb46a44f973
nick <hanxingchen1986@gmail.com>
2024-03-29 16:34:52 +0800

    提交对应的资源

--- pruemvc 2/demo1/assets/Script/core/netmgr/cmd/chat/UserSendMsgHandler.ts
+++ pruemvc 2/demo1/assets/Script/core/netmgr/cmd/chat/UserSendMsgHandler.ts
@@ -4,6 +4,6 @@ export default class UserSendMsgHandler {
     handle(oMsgBody: any) {
         console.log("收到自己发送的消息" + oMsgBody[1]);
         console.log("收到自己发送的消息" + oMsgBody[2]);
-        console.log("收到自己发送的消息" + oMsgBody[3]);
+        console.log("收到自己发送的消息" + oMsgBody[3].msg);
     }
 }
