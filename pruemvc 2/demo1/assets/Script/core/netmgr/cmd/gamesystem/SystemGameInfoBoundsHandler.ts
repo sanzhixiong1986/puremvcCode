055b5a3c2a8484faa7fd8dee7580b37ceabbcab1
nick <hanxingchen1986@gmail.com>
2024-04-10 13:43:27 +0800

    1

--- pruemvc 2/demo1/assets/Script/core/netmgr/cmd/gamesystem/SystemGameInfoBoundsHandler.ts
+++ pruemvc 2/demo1/assets/Script/core/netmgr/cmd/gamesystem/SystemGameInfoBoundsHandler.ts
@@ -1,3 +1,5 @@
+import EventManager from "../../../event/EventManager";
+
 /**
  * 领取奖励的消息
  */
@@ -5,7 +7,10 @@ export default class SystemGameInfoBoundsHandler {
 
     handle(oMsgBody: any) {
         console.log("领取奖励收到消息");
-        if (oMsgBody != 1) {
+        if (oMsgBody < 0) {
+            EventManager.getInstance().dispenseEvent({ msg_id: "errorCode", data: oMsgBody });
+        }
+        if (oMsgBody[0] == 1) {
             console.log("status err:", oMsgBody);
             return;
         }
