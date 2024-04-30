155b1fb300bed6c006ac7dcdc4cf21def90138d2
nick <hanxingchen1986@gmail.com>
2024-04-30 14:43:07 +0800

    添加了netbus的tcp的基础服务器编写，并且测试了tcp的相关操作

---
+++ cServer/class_libuvg-0,1/apps/test/main.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "../netbus/netbus.h";
+
+int main(int argc, char** argv) {
+	//½øÐÐtcpµÄ²âÊÔ
+	netbus::instance()->start_tcp_server(6080);
+	netbus::instance()->start_tcp_server(6081);
+
+
+	netbus::instance()->run();
+	
+	system("pause");
+	return 0;
+}
